c2f1931e666bf52d055ccfad3ae038c263774ee9
jishnu <jishnu2292@gmail.com>
2017-03-26 23:30:14 +0530

    popup added

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,6 @@
 <?php $this->load->view('common/header'); ?>
+		<link rel="stylesheet" type="text/css" href="assets/css/component.css" />
+
 
 			 
 						<!-- banner-text Slider starts Here -->
@@ -74,7 +76,33 @@
 						</ul>
 		</div>
 		<div class="clearfix"> </div>
-	</div>				 	
+	</div>
+	<div class="md-modal md-effect-2" id="modal-2">
+		<div class="md-content">
+			<h3>Modal Dialog right</h3>
+			<div>
+				<p>This is a modal window. You can do the following things with it:</p>
+				<ul>
+					<li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+					<li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+					<li><strong>Close:</strong> click on the button below to close the modal.</li>
+				</ul>
+				<button class="md-close">Close me!</button>
+			</div>
+		</div>
+	</div>
+	<button class="md-trigger hidden" data-modal="modal-2">Slide in (right)</button>
+	<script src="assets/js/classie.js"></script>
+		<script src="assets/js/modalEffects.js"></script>
+		<script>
+$(document).ready(function() {
+	setTimeout(function(){
+$(".md-trigger").trigger('click');
+	      
+	   },5000);
+});
+		</script>
+
 <!--/header-->
 <!-- <div class="welcome">
 	<div class="container">
